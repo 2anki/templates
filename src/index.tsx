01dd928fc04230675a3d845903aae83dea9e99a5
Alexander Alemayhu <alexander@alemayhu.com>
2022-04-11 22:14:51 +0200

    feat: fix the styling to use Bulma

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bulma/css/bulma.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

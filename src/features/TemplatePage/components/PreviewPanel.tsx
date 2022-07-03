6badf2f2515caa33810f40e77b0d1fec27f13c3d
Alexander Alemayhu <alexander@alemayhu.com>
2022-07-03 10:35:31 +0200

    refactor: set values instead of console.log

--- src/features/TemplatePage/components/PreviewPanel.tsx
+++ src/features/TemplatePage/components/PreviewPanel.tsx
@@ -36,13 +36,11 @@ export default function PreviewPanel({ template }: PreviewPanelProps) {
           style: styling,
         });
         /* @ts-ignore */
-        ed.on('update', (some, argument) => {
-          // Change!
-          console.log('eeeee', some, argument);
-          /* @ts-ignore */
-          console.log(ed.getHtml());
-          /* @ts-ignore */
-          console.log(ed.getCss());
+        ed.on('update', () => {
+          localStorage.setItem('gjs-components', JSON.stringify(ed.getComponents()));
+          localStorage.setItem('gjs-style', JSON.stringify(ed.getStyle()));
+          localStorage.setItem('gjs-html', ed.getHtml());
+          localStorage.setItem('gjs-css', ed.getCss());
         });
         setEditor(ed);
       } else {

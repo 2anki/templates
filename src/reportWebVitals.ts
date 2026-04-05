1ef0c8e6a5053520baeb6faba57de4c694110dd0
Alexander Alemayhu <alexander@alemayhu.com>
2026-04-05 13:53:45 +0200

    chore: upgrade dependencies (monaco 0.54, vite 7, web-vitals 5, react 18.3, types)

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,11 @@
-import { ReportHandler } from "web-vitals";
-
-const reportWebVitals = (onPerfEntry: ReportHandler | undefined) => {
+const reportWebVitals = (onPerfEntry?: (metric: unknown) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import("web-vitals").then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onINP(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
